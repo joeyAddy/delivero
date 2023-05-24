bfe765d62339e5d91956ca2e13e3178f49088777
joeyAddy <chatwithjohnjoseph@gmail.com>
2023-05-25 00:18:06 +0100

    Downgraded react-native-svg to solve bug

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,7 +13,7 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView className="bg-white pt-5">
       {/* header */}
       <View className="flex-row space-x-2 pb-3 items-center mx-4">
         <Image
@@ -21,10 +22,13 @@ const HomeScreen = () => {
           }}
           className="h-7 w-7 bg-grey-400 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-          <Text className="font-bold text-xl">Current Location</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
